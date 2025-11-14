973c76d47987119e24575b8448d11cf74d7dace6
aryan <22it3010@rgipt.ac.in>
2025-11-14 12:04:10 +0530

    intial commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -28,6 +28,10 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
